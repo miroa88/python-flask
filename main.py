bd6fc5711030cd551d48a7978656518c74bef175
Miro Abdalian <miroabdalian@gmail.com>
2022-03-06 11:13:56 -0800

    added area get finction

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
+import math
 from flask import Flask
 app = Flask(__name__)
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
+
+@app.route('/<int:number>')
+def getArea(number):
+    return "The circle area is " + str(number * number * math.pi)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=120)
+    app.run(host='0.0.0.0', port=5000)
